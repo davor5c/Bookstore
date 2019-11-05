bbcd4c880d4b1068c24c9d71029e2c770fca7713
Bojan Antolović <bojan.antolovic@gmail.com>
2019-11-05 11:19:55 +0100

    Modified Find-VisualStudio.ps1 to search for MSBuild.exe, to work with multiple versions of VS2015 and VS2019. This is consistent with the usage instructions for vswhere, except that older versions of vswhere do not have the 'find' argument, so this script is searching manually (optimized with 'Path' argument).

--- tools/Build/Find-VisualStudio.ps1
+++ tools/Build/Find-VisualStudio.ps1
@@ -5,7 +5,7 @@ $ErrorActionPreference = 'Stop'
 $vswhere = "${Env:ProgramFiles(x86)}\Microsoft Visual Studio\Installer\vswhere.exe"
 $vsPath = & $vswhere -latest -property installationPath
 
-$msbuild = "$vsPath\MSBuild\15.0\Bin\MSBuild.exe"
+$msbuild = Get-ChildItem -Path "$vsPath\MSBuild\*\Bin\MSBuild.exe" -Recurse | Where-Object FullName -Like '*Bin\MSBuild.exe' | Select-Object -First 1 -ExpandProperty FullName
 $vstest = "$vsPath\Common7\IDE\CommonExtensions\Microsoft\TestWindow\vstest.console.exe"
 
 return $msbuild, $vstest
