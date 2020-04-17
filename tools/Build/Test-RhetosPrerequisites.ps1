ce9b0a7560c1fa0e4844008a561cf630674d73e0
Ivan Barbančić <ibarbancic@omega-software.hr>
2020-04-17 10:04:10 +0200

    Better message when missing connection string

--- tools/Build/Test-RhetosPrerequisites.ps1
+++ tools/Build/Test-RhetosPrerequisites.ps1
@@ -1,9 +1,11 @@
-$connectionStringsPath = "$PSScriptRoot\..\..\src\Bookstore.RhetosServer\ConnectionStrings.config"
+$templateConnectionStringsPath = Resolve-Path "$PSScriptRoot\..\Configs\Templates\Template.ConnectionStrings.config"
+$serverFolderLocation = Resolve-Path "$PSScriptRoot\..\..\src\Bookstore.RhetosServer"
+$connectionStringsPath = "$serverFolderLocation\ConnectionStrings.config"
 
 if (!(Test-Path $connectionStringsPath))
 {
   $msg = "Prerequisites: Database connection string is not configured." `
-    + " Please create a Bookstore database, copy 'Template.ConnectionStrings.config' file" `
+    + " Please create a Bookstore database, copy '$templateConnectionStringsPath' file" `
     + " to '$connectionStringsPath', and setup the connection string in that file."
   throw $msg
 }
