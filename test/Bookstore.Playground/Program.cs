5f477dd0e75fd4c3b0d5421d8897d7ca79d37f86
Ivan Barbančić <ibarbancic@omega-software.hr>
2021-02-12 11:42:41 +0100

    Migrated Bookstore.Playground to .NET 5

--- test/Bookstore.Playground/Program.cs
+++ test/Bookstore.Playground/Program.cs
@@ -1,14 +1,9 @@
 ﻿using ConsoleDump;
 using Rhetos;
-using Rhetos.Dom.DefaultConcepts;
 using Rhetos.Logging;
 using Rhetos.Utilities;
-using System;
-using System.Collections.Generic;
 using System.IO;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Bookstore.Playground
 {
@@ -17,7 +12,7 @@ namespace Bookstore.Playground
         static void Main(string[] args)
         {
             ConsoleLogger.MinLevel = EventType.Info; // Use EventType.Trace for more detailed log.
-            using (var scope = new ProcessContainer(FindRhetosServerFolder()).CreateTransactionScopeContainer())
+            using (var scope = RhetosHost.CreateScope())
             {
                 var context = scope.Resolve<Common.ExecutionContext>();
                 var repository = context.Repository;
@@ -32,24 +27,6 @@ namespace Bookstore.Playground
             }
         }
 
-        /// <summary>
-        /// This utility is not executed within the main application, so we need to locate it first.
-        /// </summary>
-        private static string FindRhetosServerFolder()
-        {
-            string rhetosServerSubfolder = @"src\Bookstore.Service";
-
-            var startingFolder = new DirectoryInfo(Environment.CurrentDirectory);
-            var folder = startingFolder;
-            while (!Directory.Exists(Path.Combine(folder.FullName, rhetosServerSubfolder)))
-            {
-                if (folder.Parent == null)
-                    throw new ArgumentException($"Cannot find the Rhetos server folder '{rhetosServerSubfolder}' in '{startingFolder}' or any of its parent folders.");
-
-                folder = folder.Parent;
-            }
-
-            return Path.Combine(folder.FullName, rhetosServerSubfolder);
-        }
+        private static readonly RhetosHost RhetosHost = RhetosHost.FindBuilder(Path.GetFullPath(@"..\..\..\..\..\src\Bookstore.Service\bin\Debug\net5.0\Bookstore.Service.dll")).Build();
     }
 }
