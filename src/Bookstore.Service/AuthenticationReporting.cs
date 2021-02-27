525a519f49ff46952a8c7b311bbc463a215398ae
Bojan Antolović <bojan.antolovic@gmail.com>
2021-02-27 22:53:42 +0100

    Added DemoAuthentication.UserInfoReport.
    # Conflicts:
    #	src/Bookstore.Service/AuthenticationReporting.cs

---
+++ src/Bookstore.Service/AuthenticationReporting.cs
@@ -0,0 +1,32 @@
+﻿using Rhetos.Utilities;
+using System;
+
+namespace DemoAuthentication
+{
+    public static class AuthenticationReporting
+    {
+        public static UserInfoReport[] GetReport(IUserInfo userInfo)
+        {
+            return new UserInfoReport[]
+            {
+                new UserInfoReport { Key = "UserName", Value = GetValueOrException(() => userInfo.UserName) },
+                new UserInfoReport { Key = "Type", Value = userInfo.GetType().ToString() },
+                new UserInfoReport { Key = "IsUserRecognized", Value = GetValueOrException(() => userInfo.IsUserRecognized) },
+                new UserInfoReport { Key = "Workstation", Value = GetValueOrException(() => userInfo.Workstation) },
+                new UserInfoReport { Key = "Report", Value = GetValueOrException(() => userInfo.Report()) },
+            };
+        }
+
+        private static string GetValueOrException(Func<object> getter)
+        {
+            try
+            {
+                return getter()?.ToString();
+            }
+            catch (Exception e)
+            {
+                return $"{e.GetType().Name}: {e.Message}";
+            }
+        }
+    }
+}
