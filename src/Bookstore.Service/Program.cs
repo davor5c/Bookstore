2114d4c599a4542805b36d94a90affdda3bb9e0b
Bojan Antolović <bojan.antolovic@gmail.com>
2021-06-02 14:09:46 +0200

    Updated C# to 9.0, to support new IConceptMacro interface.

--- src/Bookstore.Service/Program.cs
+++ src/Bookstore.Service/Program.cs
@@ -20,8 +20,8 @@ namespace Bookstore.Service
                 });
 
             //Uncomment the code below to setup NLog for Dependency injection
-            //Such a setup for NLog by default will use the nlog.config file which is included as an example inide this project.
-            //This is an example of how to keep the old Rhetos logging funcionality.
+            //Such a setup for NLog by default will use the 'nlog.config' file which is included as an example inside this project.
+            //This is an example of how to keep the old Rhetos logging functionality.
             //hostBuilder.UseNLog();
 
             return hostBuilder;
